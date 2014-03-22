b4467c2e81bdb9edaa619a7827693a8f7c415c37
Ryohji IKEBE <ryohji.ikebe@gmail.com>
2014-03-22 16:29:24 +0900

    Haskell によるとてもナイーブな数独ソルバーを実装。
    とにかく置ける可能性のある数字を置いて、それが正しい盤面になるなら探索を続
    ける、そうでなければ候補から外す、を（深さ優先探索で）繰り返す。
    
    solve q0 :: [Board] とすれば全解を、 solve q0 :: Maybe [Board] とすれば最
    初に見つけた解を返す。

---
+++ solver/NumberPlace.hs
@@ -0,0 +1,72 @@
+-- NumberPlace.hs
+module NumberPlace(q0, solve) where
+
+import Data.List
+import Data.Maybe
+import Control.Monad
+
+type Board = [String] -- 9 文字ごとに 9 要素で数独を表現。
+
+q0 :: Board
+q0 = [
+ " 8    15 ",
+ "4 65 9 8 ",
+ "     8   ",
+ "         ",
+ "  2 4   3",
+ "3  8 1   ",
+ "9   7    ",
+ "6       4",
+ "15     9 "
+ ]
+
+-- 1 .. 9 を与えて行を取り出す
+row :: Board -> Int -> String
+row b n = b !! (n - 1)
+
+-- 1 .. 9 を与えて列を取り出す
+col :: Board -> Int -> String
+col b n = map (!!(n - 1)) b
+
+-- 1 .. 9 を与えて 3 x 3 の領域を取り出す。左上から右順で、順次下に。
+box :: Board -> Int -> String
+{--
+box b 1 = concat $ map (take 3.drop 0.row b) [1..3]
+box b 2 = concat $ map (take 3.drop 3.row b) [1..3]
+box b 3 = concat $ map (take 3.drop 6.row b) [1..3]
+box b 4 = concat $ map (take 3.drop 0.row b) [4..6]
+box b 5 = concat $ map (take 3.drop 3.row b) [4..6]
+box b 6 = concat $ map (take 3.drop 6.row b) [4..6]
+box b 7 = concat $ map (take 3.drop 0.row b) [7..9]
+box b 8 = concat $ map (take 3.drop 3.row b) [7..9]
+box b 9 = concat $ map (take 3.drop 6.row b) [7..9]
+--}
+box b n = let (q,r) = divMod (n-1) 3
+     in concatMap (take 3.drop (r*3).row b) [x+q*3|x<-[1..3]]
+
+valid' :: String -> Bool
+valid' s = let s' = filter (`elem` "123456789") s
+      in length s' == length (nub s')
+
+valid :: Board -> Bool
+valid b = all valid'.concat.map (flip map [1..9].($ b)) $ [box, row, col]
+
+findSp :: Board -> Maybe (Int, Int)
+findSp b = let rs = map (findIndex (==' ')) b; rIdx = findIndex (/=Nothing) rs
+       in case rIdx of
+         Nothing -> Nothing
+         Just r -> Just (1+r, 1+fromJust (rs!!r))
+
+nexts :: Board -> [Board]
+nexts b = let pos = findSp b
+  in case pos of
+    Nothing -> []
+    Just (r, c) -> let (bh, bt) = splitAt (r-1) b
+                       (rh, rt) = splitAt (c-1) (row b r)
+                       rows = map (\c -> rh ++ c:tail rt) "123456789"
+                   in map (\r -> bh ++ r:tail bt) rows
+
+solve :: (MonadPlus m) => Board -> m Board
+solve b
+      |findSp b == Nothing = if valid b then return b else mzero
+      |otherwise = msum [solve b' |b' <- nexts b, valid b']
