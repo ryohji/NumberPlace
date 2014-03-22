2197c088a66670234143da6c070a7b20df0b50b6
Ryohji IKEBE <ryohji.ikebe@gmail.com>
2014-03-23 00:31:59 +0900

    コメントを追加/修正。

--- solver/NumberPlace.hs
+++ solver/NumberPlace.hs
@@ -20,49 +20,44 @@ q0 = [
  "15     9 "
  ]
 
--- 1 .. 9 を与えて行を取り出す
+-- 1..9 を与えて行を取り出す。
 row :: Board -> Int -> String
 row b n = b !! (n - 1)
 
--- 1 .. 9 を与えて列を取り出す
+-- 1..9 を与えて列を取り出す。
 col :: Board -> Int -> String
 col b n = map (!!(n - 1)) b
 
--- 1 .. 9 を与えて 3 x 3 の領域を取り出す。左上から右順で、順次下に。
+-- 1..9 を与えて 3x3 の領域を取り出す。左上から右順で、順次下に。
 box :: Board -> Int -> String
-{--
-box b 1 = concat $ map (take 3.drop 0.row b) [1..3]
-box b 2 = concat $ map (take 3.drop 3.row b) [1..3]
-box b 3 = concat $ map (take 3.drop 6.row b) [1..3]
-box b 4 = concat $ map (take 3.drop 0.row b) [4..6]
-box b 5 = concat $ map (take 3.drop 3.row b) [4..6]
-box b 6 = concat $ map (take 3.drop 6.row b) [4..6]
-box b 7 = concat $ map (take 3.drop 0.row b) [7..9]
-box b 8 = concat $ map (take 3.drop 3.row b) [7..9]
-box b 9 = concat $ map (take 3.drop 6.row b) [7..9]
---}
 box b n = let (q,r) = divMod (n-1) 3
      in concatMap (take 3.drop (r*3).row b) [x+q*3|x<-[1..3]]
 
+-- 行、列、あるいは 3x3 領域の 9 文字中に重複する数字がないことを確認する。
 valid' :: String -> Bool
 valid' = null.(\\ "123456789").filter (`elem` "123456789")
 
+-- 盤面上の行、列、 3x3 領域のいずれにも重複数字がないことを確認する。
 valid :: Board -> Bool
 valid b = all valid'.concat.map (flip map [1..9].($ b)) $ [box, row, col]
 
+-- 盤面に空白が残っていないことを確認する。
 noSpace :: Board -> Bool
 noSpace = (==Nothing).(find (==' ')).concat
 
+-- 盤面上の最左上の空白を 1..9 の数字で置き換えた盤面列を得る。
 nexts :: Board -> [Board]
 nexts b = let cs = concat b; p = findIndex (==' ') cs  in case p of
   Nothing -> []
   Just n  -> let (h, t) = splitAt n cs
     in [slice 9 (h ++ c:tail t) | c <- "123456789"]
 
+-- List を与えた長さ n の部分列に分割し、この部分列の List を返す。
 slice :: Int -> [a] -> [[a]]
 slice n = let f xs = if null xs then mzero else (return.splitAt n) xs
   in unfoldr f
 
+-- 数独を解く。
 solve :: (MonadPlus m) => Board -> m Board
 solve b
       |noSpace b = if valid b then return b else mzero
